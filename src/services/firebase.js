e5eaa814de98fb45a187d6102cd9af2a804b412d
veronicapava <veronicapava@gmail.com>
2022-04-03 07:35:52 -0500

    Actualizacion de los datos proporcionados por firebase

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,6 +1,13 @@
-// Import the functions you need from the SDKs you need
+//Import the functions you need from the SDKs you need
+// import firebase from 'firebase/compat/app';
+// import 'firebase/compat/auth';
+// import 'firebase/compat/database';
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+
+
+//import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -16,6 +23,16 @@ const firebaseConfig = {
     measurementId: "G-VM1DQEQE5R"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app)
+export const db = getStorage(app)
+// export const db = firebase.database();
+
+// // Inicializamos la aplicación y la guardamos en firebaseApp
+// const firebaseApp = initializeApp(firebaseConfig);
+// // Exportamos firebaseApp para poder utilizarla en cualquier lugar de la aplicación
+// export default firebaseApp;
