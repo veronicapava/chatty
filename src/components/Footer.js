248f177b0b0d80ecb15e41428698f270c5a7c89e
veronicapava <veronicapava@gmail.com>
2022-04-03 07:30:20 -0500

    Logica para iniciar sesion

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <footer className="pt-5">
+            <div className="container text-center">
+                <hr />
+                <p>&copy; Chatty 2022 Todos los derechos reservados</p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
