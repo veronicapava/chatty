a9502d414965c9090b3a84f1e7b50f7ee9bc80a9
veronicapava <veronicapava@gmail.com>
2022-04-03 22:28:07 -0500

    Mejora el login y el chat. Se realiza mejora de clases a funciones

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -1,130 +1,100 @@
-import React, { Component } from 'react'
+import { useState, useEffect } from 'react'
 import Header from '../components/Header';
-import { auth } from '../services/firebase';
-import { db } from '../services/firebase';
+import { auth, db } from '../services/firebase';
+import { ref, onValue, set } from 'firebase/database'
 
-export default class Chat extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            user: auth().currentUser,
-            chats: [],
-            content: "",
-            readError: null,
-            writeError: null,
-            loadingChats: false,
-        };
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.myRef = React.createRef();
-    }
+export default function Chat() {
 
-    async componentDidMount() {
-        this.setState({ readError: null });
-        const chatArea = this.myRef.current;
+    const [user, setUser] = useState(auth.currentUser)
+    const [chats, setChats] = useState([])
+    const [content, setContent] = useState("")
+    const [error, setError] = useState(null)
+    const [writeError, setWriteError] = useState(null)
+    const [loadingChats, setLoadingChats] = useState(false)
+
+    function getChats() {
+        setError(null)
         try {
-            db.ref("chats").on("value", (snapshot) => {
-                let chats = [];
-                snapshot.forEach((snap) => {
-                    chats.push(snap.val());
-                });
-                chats.sort(function (a, b) {
-                    return a.timestamp - b.timestamp;
-                });
-                this.setState({ chats });
-                chatArea.scrollBy(0, chatArea.scrollHeight);
-                this.setState({ loadingChats: false });
-            });
+            const startCountRef = ref(db, 'chats')
+            onValue(startCountRef, (snapshot) => {
+                const data = snapshot.val()
+                setChats(data)
+            })
         } catch (error) {
-            this.setState({ readError: error.message, loadingChats: false });
+            setContent(error.target.value)
         }
     }
 
-    handleChange(event) {
-        this.setState({
-            content: event.target.value,
-        });
+    const handleOnChange = (e) => {
+        setContent(e.target.value)
     }
 
-    async handleSubmit(event) {
-        event.preventDefault();
-        this.setState({ writeError: null });
-        const chatArea = this.myRef.current;
-        let useremail = this.state.user.email;
-        let spliteo = useremail.split("@");
-        let userend = spliteo[0];
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        if (!content) {
+            return
+        } setWriteError(null)
+
         try {
-            await db.ref("chats").push({
-                content: this.state.content,
-                timestamp: Date.now(),
-                uid: this.state.user.uid,
-                user: userend,
-            });
-            this.setState({ content: "" });
-            chatArea.scrollBy(0, chatArea.scrollHeight);
-        } catch (error) {
-            this.setState({ writeError: error.message });
+            set(ref(db, 'chats/' + Date.now()), {
+                content: content,
+                timestamp: Date.now,
+                uid: user.uid
+            })
+            setContent("")
+        } catch (e) {
+            setWriteError(e.message)
         }
     }
 
-    formatTime(timestamp) {
-        const d = new Date(timestamp);
-        const time = `${d.getDate()}/${d.getMonth() + 1
-            }/${d.getFullYear()} ${d.getHours()}:${d.getMinutes()}`;
-        return time;
-    }
+    useEffect(() => {
+        getChats()
+    }, [])
 
-    render() {
-        return (
-            <div div className="chat">
-                <Header />
-                <div className="chat-area" ref={this.myRef}>
-                    {this.state.loadingChats ? (
-                        <div className="spinner-border text-success" role="status">
-                            <span className="sr-only">Loading...</span>
-                        </div>
-                    ) : (
-                        ""
-                    )}
-                    {/* chat area */}
-                    {this.state.chats.map((chat) => {
-                        return (
-                            <p
-                                key={chat.timestamp}
-                                className={
-                                    "chat-bubble " +
-                                    (this.state.user.uid === chat.uid ? "current-user" : "")
-                                }
-                            >
-                                <br />
-                                {chat.content}
-                                <br />
-                                <span className="chat-time float-right">
-                                    {this.formatTime(chat.timestamp)}
-                                </span>
-                            </p>
-                        );
-                    })}
-                </div>
-                <form onSubmit={this.handleSubmit} className="mx-3">
-                    <textarea
-                        className="form-control"
-                        name="content"
-                        onChange={this.handleChange}
-                        value={this.state.content}
-                    ></textarea>
-                    {this.state.error ? (
-                        <p className="text-danger">{this.state.error}</p>
-                    ) : null}
-                    <button className="btn btn-primary px-3 mt-2" type="submit">
-                        Enviar
-                    </button>
-                </form>
-                <div className="py-5 mx-3">
-                    Login in as:{" "}
-                    <strong className="text-info">{this.state.user.email}</strong>
-                </div>
+
+    return (
+        <div div className="chat">
+            <div className="chat-area">
+                {loadingChats ? (
+                    <div className="spinner-border text-success" role="status">
+                        <span className="sr-only">Loading chat...</span>
+                    </div>
+                ) : (
+                    ""
+                )}
+                {/* chat area */}
+                {chats.map((chat) => {
+                    return (
+                        <p key={chat.timestamp}
+                            className={
+                                "chat-bubble " +
+                                (user.uid === chat.uid ? "current-user" : "")
+                            }
+                        >
+                            <br />
+                            {chat.content}
+                            <br />
+                            <span className="chat-time float-right">
+                                {this.formatTime(chat.timestamp)}
+                            </span>
+                        </p>
+                    );
+                })}
+            </div><div className="py-5 mx-3">
+                Login in as:{" "}
+                <strong className="text-info">{user.email}</strong>
             </div>
-        );
-    }
+            <form onSubmit={handleSubmit} className="mx-3">
+                <textarea className="form-control" name="content" onChange={handleOnChange} value={content}></textarea>
+                {error ? (
+                    <p className="text-danger">{error}</p>
+                ) : null}
+                <button className="btn btn-primary px-3 mt-2" type="submit">
+                    Enviar
+                </button>
+            </form>
+
+        </div>
+    );
 }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,113 +1,62 @@
-import React, { Component } from 'react';
-import { Link } from "react-router-dom";
-import { signin } from '../helpers/auth';
+import { useState } from "react";
+import { useAuth } from "../context/authContext"
+import { Link, useNavigate } from "react-router-dom";
 
-export default class Login extends Component {
-    constructor() {
-        super();
-        this.state = {
-            error: null,
-            email: "",
-            password: ""
-        };
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-        // this.googleSignIn = this.googleSignIn.bind(this);
-        // this.githubSignIn = this.githubSignIn.bind(this);
-    }
+const Login = () => {
+    const [user, setUser] = useState({ email: "", password: "" });
+    const { login, loginWithGoogle, loginWithGitHub } = useAuth();
+    const navigate = useNavigate();
+    const [error, setError] = useState();
 
-    handleChange(event) {
-        this.setState({
-            [event.target.name]: event.target.value
-        });
-    }
+    const handleChange = ({ target: { name, value } }) => {
+        //Actualizando los estados
+        setUser({ ...user, [name]: value });
+    };
 
-    async handleSubmit(event) {
-        event.preventDefault();
-        this.setState({ error: "" });
+    const handleGoogleSignin = async () => {
         try {
-            // TODO: aca da error la funcion y entra al catch
-            await signin(this.state.email, this.state.password);
-        } catch (error) {
-            console.error(error.message);
-            this.setState({ error: error.message });
+            await loginWithGoogle();
+            navigate("/home");
+        } catch (err) {
+            console.log(err);
         }
-    }
+    };
+    const handleGitHubSignin = async () => {
+        try {
+            await loginWithGitHub();
+            navigate("/home");
+        } catch (err) {
+            console.log(err);
+        }
+    };
 
-    // async googleSignIn() {
-    //     try {
-    //         await signInWithGoogle();
-    //     } catch (error) {
-    //         this.setState({ error: error.message });
-    //     }
-    // }
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setError("");
+        try {
+            await login(user.email, user.password);
+            navigate("/home");
+        } catch (error) {
+            if (error.code === "auth/user-not-found") setError("Usuario no encontrado");
+            if (error.code === "auth/wrong-password") setError("Contraseña incorrecta");
+        }
+    };
 
-    // async githubSignIn() {
-    //     try {
-    //         await signInWithGitHub();
-    //     } catch (error) {
-    //         this.setState({ error: error.message });
-    //     }
-    // }
+    return (
+        <>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="email">Email</label>
+                <input type="email" name="email" placeholder="Your email" onChange={handleChange} />
 
-    render() {
-        return (
-            <div className="container">
-                <form
-                    className="mt-5 py-5 px-5"
-                    autoComplete="off"
-                    onSubmit={this.handleSubmit}
-                >
-                    <h1>
-                        Login to
-                        <Link className="title m-2" to="/">
-                            Chatty
-                        </Link>
-                    </h1>
-                    <p className="lead">
-                        Fill in the form below to login to your account.
-                    </p>
-                    <div className="form-group">
-                        <input
-                            className="form-control m-2"
-                            placeholder="Email"
-                            name="email"
-                            type="email"
-                            onChange={this.handleChange}
-                            value={this.state.email}
-                        />
-                    </div>
-                    <div className="form-group">
-                        <input
-                            className="form-control m-2"
-                            placeholder="Password"
-                            name="password"
-                            onChange={this.handleChange}
-                            value={this.state.password}
-                            type="password"
-                        />
-                    </div>
-                    <div className="form-group m-2">
-                        {this.state.error ? (
-                            <p className="text-danger">{this.state.error}</p>
-                        ) : null}
-                        <button className="btn btn-primary px-5" type="submit">Login</button>
-                    </div>
-                    <br />
-                    <p className='m-2'>You can also log in with any of these services</p>
-                    {/* <button className="btn btn-secondary m-2" type="button" onClick={this.googleSignIn}>
-                        Sign in with Google
-                    </button>
-                    <button className="btn btn-secondary" type="button" onClick={this.githubSignIn}>
-                        Sign in with GitHub
-                    </button>
-                    <hr /> */}
-                    <p>
-                        Don't have an account? <Link to="/signup">Sign up</Link>
-                    </p>
-                </form>
+                <label htmlFor="password">Password</label>
+                <input type="password" name="password" placeholder="Your password" onChange={handleChange} />
 
-            </div>
-        );
-    }
-}
+                <button>Login</button>
+            </form>
+            {error && <p>{error}</p>}
+            <button onClick={handleGoogleSignin}>Login With Google Account</button>
+        </>
+    );
+};
+
+export default Login;
